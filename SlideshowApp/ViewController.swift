b138767117b83c859bb265ffb5bfba477d1a1fbd
小本裕也 <komotoyuuya@komotoyuuya-no-imac.local>
2017-02-23 19:32:38 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 小本裕也 on 2017/02/17.
+//  Copyright © 2017年 小本裕也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    let imageArray = ["Image-1","Image-2","Image-3"]
+    var count = 0
+    var timer:Timer!
+
+    @IBAction func susumu(_ sender: Any) {
+ count += 1
+        if (count > 2){
+            count = 0
+        }
+    imageView.image = UIImage(named:imageArray[count])
+    }
+   
+    @IBAction func modoru(_ sender: Any) {
+    count -= 1
+        if(count < 0){
+            count = 2
+        }
+    imageView.image = UIImage(named:imageArray[count])
+    }
+    
+    
+    @IBAction func StartStop(_ sender: Any) {
+    
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+        else {
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    }
+    
+        func updateTimer() {
+            count += 1
+            if ( count > 2 ) {
+                count = 0
+            }
+            imageView.image = UIImage(named:imageArray[count])
+        }
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        imageView.image = UIImage(named:imageArray[0])
+    }
+
+    
+    @IBAction func tapped(_ sender: Any) {
+        
+        
+        }
+ 
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func prepare(for segue:UIStoryboardSegue, sender:Any?){
+        let viewcontroller2:ViewController2 = segue.destination as! ViewController2
+        viewcontroller2.img = "Image-1,Image-2,Image-3"
+    }
+ 
+    
+
+    @IBAction func modoru(segue:UIStoryboardSegue){
+    }
+    
+    
+}
+
---
+++ SlideshowApp/ViewController2.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController2.swift
+//  SlideshowApp
+//
+//  Created by 小本裕也 on 2017/02/17.
+//  Copyright © 2017年 小本裕也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+ 
+    @IBOutlet weak var imageView2: UIImageView!
+
+    var img:String!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      imageView2.image = UIImage(named:(img))
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
